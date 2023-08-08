deceb9d8bedb27aed86d9852ac23add46ccaecc7
廖文忠 <wcliao1962@gmail.com>
2023-08-08 14:21:02 +0800

    修正posts外來鍵為user_id

--- database/migrations/2023_08_02_125742_create_posts_table.php
+++ database/migrations/2023_08_02_125742_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('post_id');
+            $table->bigInteger('user_id');
             $table->timestamps();
         });
     }
