2202650d15e71d5a51f9bca593a1c7c316331389
廖文忠 <wcliao1962@gmail.com>
2023-08-10 16:35:36 +0800

    新增出貨狀態(shipped_status)欄位

--- database/migrations/2023_08_08_061040_create_orders_table.php
+++ database/migrations/2023_08_08_061040_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
+            $table->tinyInteger('shipped_status');
             $table->timestamps();
         });
     }
